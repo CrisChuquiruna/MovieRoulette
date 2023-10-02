5434bc4c4c29ab1d753ca7b7ba388c4e10c86211
Cristian <cris.chuquiruna@gmail.com>
2023-10-02 20:10:42 -0300

    change cors allows

--- movie_roulette_api/settings.py
+++ movie_roulette_api/settings.py
@@ -35,12 +35,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1",
+    "http://youmovieroulette.netlify.app/"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1",
+    "http://youmovieroulette.netlify.app/"
 ]
 
 if RENDER_EXTERNAL_HOSTNAME:    
