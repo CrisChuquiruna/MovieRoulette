70227acd2ba603d4a41057b85654c812ec3ab28b
Cristian <cris.chuquiruna@gmail.com>
2023-10-03 09:22:03 -0300

    csrf trusted origins add new url

--- movie_roulette_api/settings.py
+++ movie_roulette_api/settings.py
@@ -29,7 +29,7 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
