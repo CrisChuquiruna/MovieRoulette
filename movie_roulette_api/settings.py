b691b5d2c9faa656827d06495547215059d26ef8
Cristian <cris.chuquiruna@gmail.com>
2023-10-03 08:09:45 -0300

    csrf trusted origins add new url

--- movie_roulette_api/settings.py
+++ movie_roulette_api/settings.py
@@ -35,14 +35,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1",
-    "https://youmovieroulette.netlify.app/"
+    "https://youmovieroulette.netlify.app"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1",
-    "https://youmovieroulette.netlify.app/"
+    "https://youmovieroulette.netlify.app"
 ]
 
 if RENDER_EXTERNAL_HOSTNAME:    
