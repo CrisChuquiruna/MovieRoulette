957094e5833b0b1aa950c450714ebf9d31107c8f
Cristian <cris.chuquiruna@gmail.com>
2023-10-11 14:03:50 -0300

    change

--- movie_lists/views.py
+++ movie_lists/views.py
@@ -3,11 +3,15 @@ from .serializer import MovieListsSerializer
 from .models import MovieLists
 from django.http import JsonResponse
 from rest_framework import permissions
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
 
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+    def enforce_csrf(self, request):
+        return  # To not perform the csrf check previously happening
 # Create your views here.
 class MovieListView(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication,)
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (permissions.AllowAny,)
     serializer_class = MovieListsSerializer
     queryset = MovieLists.objects.all()
