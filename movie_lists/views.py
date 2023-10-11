569d776f2f211adc4f43929576c96da957439ce8
Cristian <cris.chuquiruna@gmail.com>
2023-10-11 14:09:38 -0300

    chango bassicAuth

--- movie_lists/views.py
+++ movie_lists/views.py
@@ -3,15 +3,11 @@ from .serializer import MovieListsSerializer
 from .models import MovieLists
 from django.http import JsonResponse
 from rest_framework import permissions
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
+from rest_framework.authentication import SessionAuthentication
 
-class CsrfExemptSessionAuthentication(SessionAuthentication):
-
-    def enforce_csrf(self, request):
-        return  # To not perform the csrf check previously happening
 # Create your views here.
 class MovieListView(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    authentication_classes = (SessionAuthentication)
     permission_classes = (permissions.AllowAny,)
     serializer_class = MovieListsSerializer
     queryset = MovieLists.objects.all()
